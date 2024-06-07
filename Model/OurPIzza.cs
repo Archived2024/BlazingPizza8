591a33a46474434b5279054097a1c958052b9a1c
AnjinYamato <paul.tannenberg@gmail.com>
2024-06-07 13:21:08 +0200

    Added ourpizza class

---
+++ Model/OurPIzza.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazingPizza8.Model
+{
+    public class OurPIzza
+    {
+        public int PizzaId { get; set; }
+        public string Name { get; set; }
+        public string Descrirption { get; set; }
+        public decimal Price { get; set; }
+        public bool Vegetarian { get; set; }
+        public bool Vegan { get; set; }
+
+
+    }
+}
+
